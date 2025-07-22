00e27f8f52528cd125715682c1a8ea83d6158c9b
Jessica Cayaban <jest.cayaban@gmail.com>
2025-07-23 08:45:33 +0900

    added file output

--- src/utils/io.rs
+++ src/utils/io.rs
@@ -1,12 +1,14 @@
 use std::fs;
+use std::io::{Result, Write};
 
-pub fn open_line_breaks(filename: &str) -> Vec<String> {
+pub fn open_line_breaks(filename: &str) -> String {
     let contents = fs::read_to_string(filename)
         .expect("Should have been able to read the file");
-    let mut res = Vec::new();
-    for i in contents.lines() {
-        print!("{i}\n");
-        res.push(i.to_string());
-    }
-    return res;
+    return contents;
+}
+
+pub fn write_file(filename: &str, contents: &str) -> Result<()> {
+    let mut file = fs::File::create(format!("{}.txt", filename.to_string()))?;
+    let _ = file.write_all(contents.as_bytes());
+    Ok(())
 }
