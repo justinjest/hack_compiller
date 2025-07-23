9ecdaf33e8700d1f2cb9fcc30d301ee8c3868ddb
Jessica Cayaban <jest.cayaban@gmail.com>
2025-07-23 16:34:59 +0900

    Set up code.rs and removed utils folder

---
+++ src/code.rs
@@ -0,0 +1,13 @@
+pub fn a_command (val: &str) -> u16 {
+    return 0b0000_0000_0000_0000
+}
+
+
+pub fn c_command (val: &str) -> u16 {
+    return 0b0000_0000_0000_0000
+}
+
+
+pub fn l_command (val: &str) -> u16 {
+    return 0b0000_0000_0000_0000
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 mod utils;
 mod parser;
+mod code;
 
 use crate::utils::io;
 
 fn main() {
     println!("Hello, world!");
     let _ = parser::process_file("./resources/test.asm");
+    let t = code::a_command("");
+    print!("{t}");
 }
