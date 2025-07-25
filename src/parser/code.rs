542e9e3caa85e7c17a1d1ca5504629a216b2525c
Jessica Cayaban <jest.cayaban@gmail.com>
2025-07-25 15:08:13 +0900

    Created comp table

--- src/parser/code.rs
+++ src/parser/code.rs
@@ -49,8 +49,41 @@ fn combine_c(comp: u16, dest: u16, jmp: u16) -> u16 {
 }
 
 // The following functions lookup what the int for the relevent symbol is
-fn create_comp(_comp: &str) -> u16 {
-    return 0
+fn create_comp(comp: &str) -> u16 {
+    let comp_table = HashMap::from([
+        ("0",   0b0101010),
+        ("1",   0b0111111),
+        ("-1",  0b0111010),
+        ("D",   0b0001100),
+        ("A",   0b0110000),
+        ("M",   0b1110000),
+        ("!D",  0b0001101),
+        ("!A",  0b0110001),
+        ("!M",  0b1110001),
+        ("-D",  0b0001111),
+        ("-A",  0b0110011),
+        ("-M",  0b1110011),
+        ("D+1", 0b0011111),
+        ("A+1", 0b0110111),
+        ("M+1", 0b1110111),
+        ("D-1", 0b0001110),
+        ("A-1", 0b0110010),
+        ("M-1", 0b1110010),
+        ("D+A", 0b0000010),
+        ("D+M", 0b1000010),
+        ("D-A", 0b0010011),
+        ("D-M", 0b1010011),
+        ("A-D", 0b0000111),
+        ("M-D", 0b1000111),
+        ("D&A", 0b0000000),
+        ("D&M", 0b1000000),
+        ("D|A", 0b0010101),
+        ("D|M", 0b1010101)
+    ]);
+    match comp_table.get(comp) {
+        Some(val) => return *val,
+        None => panic!("Invalid comp recieved")
+    }
 }
 
 fn create_dest(dest: &str) -> u16 {
@@ -228,7 +261,7 @@ mod tests {
     #[test]
     fn test_create_jmp_jmp() {
         let res = create_jmp(&"JMP");
-        assert_eq!(res, 7);
+        assert_eq!(res, 0b111);
     }
 
 }
